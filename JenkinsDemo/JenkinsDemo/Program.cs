a369161a356ac968d93db4f80e61fa11a5218577
Rohith <rohith_chidura@epam.com>
2022-08-24 00:48:04 +0530

    First Commit

---
+++ JenkinsDemo/JenkinsDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace JenkinsDemo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
